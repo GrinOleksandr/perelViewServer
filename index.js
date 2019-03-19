15bb0a97ddc33815e89dbc4397740906178c5a65
GrinOleksandr <sasha.lisove@gmail.com>
2019-03-19 12:12:34 +0200

    now returns if call center is closed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port =  process.env.PORT || 12345;
 const formidableMiddleware = require('express-formidable');
 const date = require('date-and-time');
 const cors = require('cors');
+const moment = require('moment');
 
 app.get('/', (req, res) => {
     res.end("This is Oleksandr's server! ;)");
@@ -13,28 +14,26 @@ app.get('/', (req, res) => {
 
 //Handling time requests
 
-app.options('/time', cors());
-app.get('/time', cors(),  (req, res) => {
+app.options('/isclosed', cors());
+app.get('/isclosed', cors(),  (req, res) => {
     console.log('time request recieved');
-    let now = new Date();
-    let data = {
-        location: "Israel-Jerusalem",
-        timezone: "[GMT]+2",
-        time: date.format(now, 'YYYY:MM:DD:dddd:HH:mm:ss [GMT]+5')
-        // year: date.format(now, 'YYYY [GMT]+2'),
-        // month: date.format(now, 'MM [GMT]+2'),
-        // date: date.format(now, 'DD [GMT]+2'),
-        // day: date.format(now, 'dddd [GMT]+2'),
-        // hours: date.format(now, 'HH [GMT]+7'),
-        // minutes: date.format(now, 'mm [GMT]+2'),
-        // seconds: date.format(now, 'ss [GMT]+2')
-    };
+
+    let israelTime = moment().utcOffset('+02:00').format('dddd h');
+    let timeData = israelTime.split(' ');
+    let day= timeData[0];
+    let hour=timeData[1];
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200);
 
-    res.end(JSON.stringify(data));
+    if(((day === "Friday") && (8 >= +hour >= 12)
+        || (hour >= 8 >= 17))){
+        res.end("true")
+    }
+
+    else res.end("false")
+
 });
 
 
